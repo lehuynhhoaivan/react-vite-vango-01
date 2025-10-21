20639a3642a2b403b9e06ac6975cd3e3692fd101
unknown <lehuynhhoaivan2001@gmail.com>
2025-10-22 04:37:45 +0700

    Chạy console.log của hàm myFunction()

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
+  function myFunction() {
+    console.log('Hello Vango')
+  }
+  myFunction();
+
   return (
     <>
       <div>
