8e7df9373a889e2ccc3f90bb4fa93570e5b1b0c5
unknown <lehuynhhoaivan2001@gmail.com>
2025-10-24 21:57:05 +0700

    Thêm Fragment <></> để gộp nhiều thẻ vào 1 thẻ cha

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,7 +4,10 @@ import styles from '../../components/css/MyComponent.module.css'
 // component = html + css + js
 const MyComponent = () => {
   return (
-    <h4 className={styles.textName}>VANGO</h4>
+    <>
+      <h4 className={styles.textName}>VANGO</h4>
+      <h4 className={styles.textName}>BOIT</h4>
+    </>
   )
 }
 
