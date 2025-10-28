25613137ceb2bbd584f43f24732e19755ebfe217
unknown <lehuynhhoaivan2001@gmail.com>
2025-10-28 09:44:21 +0700

    props (tt01)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
-function Header(props) {
-    console.log('Header props:', props)
+// Nên dùng
+function Header({name}) {
     return (
             <>
                 <h1>Component Header</h1>
-                <div style={{ color: 'red' }}>My name is { props.name }</div>
+                <div style={{ color: 'red' }}>My name is { name }</div>
             </>
 
     )
 }
 
 export default Header
+
+
+// Cách này hạn chế dùng (props)
+// function Header(props) {
+        // props là 1 object {}
+        // {
+        //     name: 'Vango',
+        //     age: 18,
+        //     data: [1,2,3,4,5]
+        // }
+//     const { name, age, dada } = props
+//    const name = props.name
+//    const age = props.age
+//    const data = props.data
+//     console.log('Header props:', props)
+//     return (
+//             <>
+//                 <h1>Component Header</h1>
+//                 <div style={{ color: 'red' }}>My name is { props.name }</div>
+//             </>
+
+//     )
+// }
+
+// export default Header
